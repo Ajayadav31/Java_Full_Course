f03013c70a28bef97873f0b76387bdf759c02261
Ajay <ajayadav3108@gmail.com>
2023-11-17 13:12:38 +0530

    ArrayLIst[half]

---
+++ src/threading/ArrayList1.java
@@ -0,0 +1,54 @@
+package threading;
+
+import java.util.*;
+public class ArrayList1 {
+	public static void main(String[] args) {
+		//Constructor 1
+		ArrayList<Integer> Array1 = new ArrayList<>();
+		Array1.add(100);
+		//Constructor 2
+		ArrayList<Integer> Array2 = new ArrayList<>(5);
+		Array2.add(300);
+		
+		//Constructor 3
+		Collection<Integer> My = new ArrayList<>();
+		My.add(100);
+		
+		ArrayList<Integer> Array3 = new ArrayList<>(My);
+	
+		//Methods
+		
+	/*	Array1.add(0, 100);
+		Array1.add(200);
+	//	Array1.addAll(5, Array3);
+		Array1.addAll(Array2);
+		Array1.addAll(0, My);
+		Array1.addAll(6, Array3);
+		
+		My.clear();
+		for(int item : Array1) {
+			System.out.println(item);
+		}
+	*/
+		
+		
+	/*	Array3.clear();
+		for(int item : Array3) {
+			System.out.println(item);
+		}
+		My.clear();
+		for(int item : My) {
+			System.out.println(item);
+		}
+	*/	
+		
+	/*	
+		System.out.println(Array1.contains(10));
+		System.out.println(Array1.contains(Array3));
+		System.out.println(Array1.contains(My));
+	*/	
+		
+		Array2.ensureCapacity(10);
+		System.out.println(Array1.equals(Array3));
+	}
+}
