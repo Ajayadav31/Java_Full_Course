b24f2f8921eaba2c2456351049377441a2bd21d0
Ajay <ajayadav3108@gmail.com>
2023-11-25 14:54:36 +0530

    gregianCalender git commit -m gregianCalender git commit -m gregianCalender

--- src/advanceJava/GergianCalendar.java
+++ src/advanceJava/GergianCalendar.java
@@ -4,14 +4,14 @@ import java.util.TimeZone;
 
 public class GergianCalendar {
 	public static void main(String[] args) {
-		GregorianCalendar c = new GregorianCalendar();
-		System.out.println(c.isLeapYear(1996));
+		GregorianCalendar c1 = new GregorianCalendar();
+		System.out.println(c1.isLeapYear(1996));
 		for(int i = 0;i<631;i++) {
 			System.out.println(TimeZone.getAvailableIDs()[i]);
 		}
-		System.out.println(c.toZonedDateTime());
-		System.out.println(c.getAvailableCalendarTypes());
+		System.out.println(c1.toZonedDateTime());
 		
+		System.out.println(TimeZone.getDefault());
 	}
 
 }
