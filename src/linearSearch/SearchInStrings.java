56b8b3875a93dafcf9343bdeecc544a3c60c9596
Ajay <ajayadav3108@gmail.com>
2024-02-17 22:32:43 +0530

    adding inhance for loop

--- src/linearSearch/SearchInStrings.java
+++ src/linearSearch/SearchInStrings.java
@@ -14,8 +14,13 @@ public class SearchInStrings {
 			if(str.length()==0) {
 				return false;
 			}
-			for(int i=0;i<str.length();i++) {
+			/*for(int i=0;i<str.length();i++) {
 				if(target==str.charAt(i)) {
+					return true;
+				}
+			}*/
+			for(char ch : str.toCharArray()) {
+				if(ch==target) {
 					return true;
 				}
 			}
